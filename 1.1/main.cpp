fed680e81b266ef915a6ec15866ae99d707d5c59
Ivashkin58 <115586011+Ivashkin58@users.noreply.github.com>
2023-12-05 22:06:46 +0300

    main.cpp

--- 1.1/main.cpp
+++ 1.1/main.cpp
@@ -12,30 +12,36 @@ bool isValid(const wstring& s)
 }
 int main(int argc, char **argv)
 {
-    int klych=3;
     locale loc("ru_RU.UTF-8");
     locale::global(loc);
-    wstring z;
+    wstring key;
     wstring text;
     unsigned op;
-    wcout<<L"Пожалуйста введите пароль: ";
-    wcin>>z;
-    Cipher cipher(klych);
+    wcout<<L"Cipher ready. Input key: ";
+    wcin>>key;
+    if (!isValid(key)) {
+        wcerr<<L"key not valid\n";
+        return 1;
+    }
+    wcout<<L"Key loaded\n";
+    modAlphaCipher cipher(key);
     do {
-        wcout<<L"Какую операцию необходимо произветси(0-exit, 1-encrypt, 2-decrypt): ";
+        wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
         wcin>>op;
-        if (isValid(text)) {
-            if (op >0) {
-            wcout<<L"Введите текст: ";
+        if (op > 2) {
+            wcout<<L"Illegal operation\n";
+        } else if (op >0) {
+            wcout<<L"Cipher ready. Input text: ";
             wcin>>text;
+            if (isValid(text)) {
                 if (op==1) {
-                    wcout<<L"Закодированный текст: "<<cipher.zakodirovatCipher(cipher, text)<<endl;
+                    wcout<<L"Encrypted text: "<<cipher.encrypt(text)<<endl;
                 } else {
-                    wcout<<L"Раскодированный текст: "<<cipher.raskodirovatCipher(cipher, text)<<endl;
+                    wcout<<L"Decrypted text: "<<cipher.decrypt(text)<<endl;
                 }
-                }else {
+            } else {
                 wcout<<L"Operation aborted: invalid text\n";
-                }
+            }
         }
     } while (op!=0);
     return 0;
